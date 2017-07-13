68c2c88e02a9e8d4284cdc66771a86c0d87435a6
James R. Vaughan <j_r_vaughan@yahoo.com>
2017-07-12 20:30:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 	
-	<title>PHP Study</title>
+	<title>JRV_PHP_Study</title>
   </head>
   <body>
 	<div class="container">
-	PHP Study <br/><br/>
+	JRV_PHP_Study <br/><br/>
 		<?php
 			echo "== Variables ==<br/>";
 			
